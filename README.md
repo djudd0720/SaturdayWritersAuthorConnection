46dd38d46e41c8a89246a73c7ecbd903f3e356a0
Denise Judd <djudd0720@yahoo.com>
2020-06-09 16:51:10 -0400

    added user registration and json web token code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SaturdayWritersAuthorConnection
+
+MERN stack for writers and authors to connect and share
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config"); //to gain access to jwtSecret
+
 //bring in express validator to validate user data
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
+
+//bring in user model
+const User = require("../../models/User");
 
 //@route  GET api/users
 //@desc   Register user
@@ -17,12 +25,63 @@ router.post(
       "Please enter a password with 8 or more characters"
     ).isLength({ min: 8 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User route");
+
+    //see if user exists using async await
+    const { name, email, password } = req.body;
+
+    try {
+      //user exists?, get users gravatar, encrypt password, return jsontoken
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save(); //using await instead of .then etc. and it gives a promise
+
+      //create payload for jsonwebtoken pkg
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"), //pass in the secret
+        { expiresIn: 360000000 }, //change to 3600 in prod (1 hour)
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
