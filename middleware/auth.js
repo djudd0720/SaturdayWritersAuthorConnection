52e32722883f901c9aec96efc7230a8620237bc5
Denise Judd <djudd0720@yahoo.com>
2020-06-09 17:56:08 -0400

    added custom middleware & jwt verification

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+//verify jsonwebtoken
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function (req, res, next) {
+  //GET token from header
+  const token = req.header("x-auth-token");
+
+  //check if no token
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+  //Verify token
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+
+    req.user = decoded.user;
+    next();
+  } catch {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
